5d4553bea65256d5bff440e80453b682d2470b17
xoriath <moro.engelhardt@gmail.com>
2016-07-31 21:56:58 +0200

    We're now stopping on the reset vector (which does not have source from avr-libc). Need to continue to main

--- src/atmelDebugAdapter.ts
+++ src/atmelDebugAdapter.ts
@@ -7,6 +7,7 @@ import {
 	InitializedEvent,
 	TerminatedEvent,
 	StoppedEvent,
+	ContinuedEvent,
 	BreakpointEvent,
 	OutputEvent,
 	Event,
@@ -79,6 +80,7 @@ export interface LaunchRequestArguments extends DebugProtocol.LaunchRequestArgum
 	packPath: string;
 }
 
+
 class DeviceListener implements IDeviceListener {
 	public processService: ProcessesService;
 	public module: string;
@@ -113,46 +115,7 @@ class DeviceListener implements IDeviceListener {
 	}
 }
 
-class RunControlLaunchListener implements IRunControlListener {
-
-	private service: RunControlService;
-
-	public constructor(service: RunControlService) {
-		this.service = service;
-	}
-	public contextAdded(contexts: RunControlContext[]): void {
-	}
-	public contextChanged(contexts: RunControlContext[]): void {
-	}
-	public contextRemoved(contextIds: string[]): void {
-	}
-	public contextSuspended(contextId: string, pc: number, reason: string, state: any): void {
-		let context = <RunControlContext>this.service.contexts[contextId];
-		switch (reason) {
-			case "Reset":
-				context.resume(ResumeMode.UntilActive);
-				break;
-			case "Step":
-
-				break;
-
-			default:
-				break;
-		}
-	}
-	public contextResumed(contextId: string): void {
-	}
-	public contextException(contextId: string, description: string): void {
-	}
-	public containerSuspended(contextId: string, pc: number, reason: string, state: any, contextIds: string[]): void {
-	}
-	public containerResumed(contextIds: string[]): void {
-	}
- 	public contextStateChanged(contextIds: string[]): void {
-	}
-}
-
-class AtmelDebugSession extends DebugSession {
+class AtmelDebugSession extends DebugSession implements IRunControlListener {
 
 	public constructor() {
 		super();
@@ -166,10 +129,10 @@ class AtmelDebugSession extends DebugSession {
 	private services: Map<string, IService> = new Map<string, IService>();
 
 	protected initializeRequest(response: DebugProtocol.InitializeResponse, args: DebugProtocol.InitializeRequestArguments): void {
-
+		this.log("initializeRequest");
 		// this.sendEvent(new InitializedEvent());
 
-		response.body.supportsConfigurationDoneRequest = true;
+		response.body.supportsConfigurationDoneRequest = false;
 		response.body.supportsEvaluateForHovers = true;
 		response.body.supportsFunctionBreakpoints = true;
 		response.body.supportsSetVariable = true;
@@ -178,20 +141,26 @@ class AtmelDebugSession extends DebugSession {
 	}
 
     protected disconnectRequest(response: DebugProtocol.DisconnectResponse, args: DebugProtocol.DisconnectArguments): void {
-		let processService = <ProcessesService>this.services["Processes"];
-		for (let index in processService.contexts) {
-			let context = <IProcessesContext>processService.contexts[index];
-			context.terminate();
+		this.log("disconnectRequest");
+		if ("Processes" in this.services) {
+			let processService = <ProcessesService>this.services["Processes"];
+			for (let index in processService.contexts) {
+				let context = <IProcessesContext>processService.contexts[index];
+				context.terminate();
+			}
 		}
 
-		let toolService = <ToolService>this.services["Tools"];
-		for (let index in toolService.contexts) {
-			let context = <IToolContext>toolService.contexts[index];
-			context.tearDownTool();
+		if ("Tools" in this.services) {
+			let toolService = <ToolService>this.services["Tools"];
+			for (let index in toolService.contexts) {
+				let context = <IToolContext>toolService.contexts[index];
+				context.tearDownTool();
+			}
 		}
 	}
 
     protected launchRequest(response: DebugProtocol.LaunchResponse, args: LaunchRequestArguments): void {
+		this.log("launchRequest");
 		this.dispatcher = new Dispatcher(args.atbackendHost, args.atbackendPort, (message: string) => {
 			this.sendEvent(new OutputEvent(message));
 		});
@@ -245,36 +214,37 @@ class AtmelDebugSession extends DebugSession {
 
 				toolService.addListener(toolListener);
 				deviceService.addListener(new DeviceListener(args.program, processService));
-				runControlService.addListener(new RunControlLaunchListener(runControlService));
+				runControlService.addListener(this);
 
 				toolService.setupTool(args.tool, "", {});
 			});
 		});
 
+		this.sendResponse(response);
 
 	}
 
     protected attachRequest(response: DebugProtocol.AttachResponse, args: DebugProtocol.AttachRequestArguments): void {
-
+		this.log("attachRequest");
 	}
 
     protected setBreakPointsRequest(response: DebugProtocol.SetBreakpointsResponse, args: DebugProtocol.SetBreakpointsArguments): void {
-
+		this.log("setBreakPointsRequest");
 	}
 
     protected setFunctionBreakPointsRequest(response: DebugProtocol.SetFunctionBreakpointsResponse, args: DebugProtocol.SetFunctionBreakpointsArguments): void {
-
+		this.log("setFunctionBreakPointsRequest");
 	}
 
     protected setExceptionBreakPointsRequest(response: DebugProtocol.SetExceptionBreakpointsResponse, args: DebugProtocol.SetExceptionBreakpointsArguments): void {
-
+		this.log("setExceptionBreakPointsRequest");
 	}
 
     protected configurationDoneRequest(response: DebugProtocol.ConfigurationDoneResponse, args: DebugProtocol.ConfigurationDoneArguments): void {
-
+		this.log("configurationDoneRequest");
 	}
 
-	private resume(threadID: number, mode: ResumeMode): void {
+	private resume(mode: ResumeMode, threadID?: number): void {
 		let runControlService = <RunControlService>this.services["RunControl"];
 
 		// TODO, support threads?
@@ -285,79 +255,126 @@ class AtmelDebugSession extends DebugSession {
 		}
 	}
 
+	private suspend(threadID?: number): void {
+		let runControlService = <RunControlService>this.services["RunControl"];
+
+		// TODO, support threads?
+
+		for (let index in runControlService.contexts) {
+			let context = <RunControlContext>runControlService.contexts[index];
+			context.suspend();
+		}
+	}
+
     protected continueRequest(response: DebugProtocol.ContinueResponse, args: DebugProtocol.ContinueArguments): void {
-		this.resume(args.threadId, ResumeMode.Resume);
+		this.log("continueRequest");
+		this.resume(ResumeMode.Resume, args.threadId);
 
 		response.body.allThreadsContinued = true;
 		this.sendResponse(response);
 	}
 
     protected nextRequest(response: DebugProtocol.NextResponse, args: DebugProtocol.NextArguments): void {
-		this.resume(args.threadId, ResumeMode.StepOver);
+		this.log("nextRequest");
+		this.resume(ResumeMode.StepOver, args.threadId);
 
 		response.body.allThreadsContinued = true;
 		this.sendResponse(response);
 	}
 
     protected stepInRequest(response: DebugProtocol.StepInResponse, args: DebugProtocol.StepInArguments): void {
-		this.resume(args.threadId, ResumeMode.StepInto);
+		this.log("stepInRequest");
+		this.resume(ResumeMode.StepInto, args.threadId);
 
 		response.body.allThreadsContinued = true;
 		this.sendResponse(response);
 	}
 
     protected stepOutRequest(response: DebugProtocol.StepOutResponse, args: DebugProtocol.StepOutArguments): void {
-		this.resume(args.threadId, ResumeMode.StepOut);
+		this.log("stepOutRequest");
+		this.resume(ResumeMode.StepOut, args.threadId);
 
 		response.body.allThreadsContinued = true;
 		this.sendResponse(response);
 	}
 
     protected stepBackRequest(response: DebugProtocol.StepBackResponse, args: DebugProtocol.StepBackArguments): void {
-
+		this.log("stepBackRequest");
 	}
 
     protected pauseRequest(response: DebugProtocol.PauseResponse, args: DebugProtocol.PauseArguments): void {
-
+		this.log("pauseRequest");
+		this.suspend(args.threadId);
 	}
 
     protected sourceRequest(response: DebugProtocol.SourceResponse, args: DebugProtocol.SourceArguments): void {
-
+		this.log("sourceRequest");
 	}
 
 	private hashString(str: string): number {
-		return str.split("").reduce( function(a, b) {
-										a = ( (a<<5) - a ) + b.charCodeAt(0);
-										return a & a
-									}, 0);
+		return Math.abs(str.split("").reduce(function (a, b) {
+			a = ((a << 5) - a) + b.charCodeAt(0);
+			return a & a
+		}, 0));
 	}
 
     protected threadsRequest(response: DebugProtocol.ThreadsResponse): void {
-
+		this.log("threadsRequest");
 		let processService = <ProcessesService>this.services["Processes"];
 
 		let threads = [];
 		for (let index in processService.contexts) {
 			let context = <IProcessesContext>processService.contexts[index];
 			threads.push(new Thread(
-				this.hashString(context.ID),
+				this.hashString(context.RunControlId),
 				context.Name));
 		}
 
+		if (!response.body) {
+			response.body = {
+				threads
+			};
+		}
 		response.body.threads = threads;
 		this.sendResponse(response);
 	}
 
     protected stackTraceRequest(response: DebugProtocol.StackTraceResponse, args: DebugProtocol.StackTraceArguments): void {
+		this.log("stackTraceRequest");
 
+		if (!response.body) {
+			response.body = {
+				stackFrames: [],
+				totalFrames: 0
+			};
+		}
+
+		let stackTraceService = <StackTraceService>this.services["StackTrace"];
+		let processesService = <ProcessesService>this.services["Processes"];
+
+		let threadId = args.threadId; // TODO; support threads
+		for (let index in processesService.contexts) {
+			let processContext = <IProcessesContext>processesService.contexts[index];
+
+			stackTraceService.getChildren(processContext.ID, (children) => {
+				stackTraceService.getContext(children, (frames) => {
+					frames.forEach(frame => {
+						response.body.stackFrames.push(new StackFrame(this.hashString(frame.ID), frame.Func, new Source(frame.File, frame.File), frame.Line));
+					});
+
+					this.sendResponse(response);
+				});
+			});
+		}
 	}
 
     protected scopesRequest(response: DebugProtocol.ScopesResponse, args: DebugProtocol.ScopesArguments): void {
-
+		this.log("scopesRequest");
+		args.frameId
 	}
 
     protected variablesRequest(response: DebugProtocol.VariablesResponse, args: DebugProtocol.VariablesArguments): void {
-
+		this.log("variablesRequest");
 	}
 
     protected setVariableRequest(response: DebugProtocol.SetVariableResponse, args: DebugProtocol.SetVariableArguments): void {
@@ -365,6 +382,83 @@ class AtmelDebugSession extends DebugSession {
 	}
 
     protected evaluateRequest(response: DebugProtocol.EvaluateResponse, args: DebugProtocol.EvaluateArguments): void {
+		this.log("evaluateRequest");
+	}
+
+	// IRunControlListener
+	public contextAdded(contexts: RunControlContext[]): void {
+
+	}
+
+	public contextChanged(contexts: RunControlContext[]): void {
+
+	}
+
+	public contextRemoved(contextIds: string[]): void {
+
+	}
+
+	public contextSuspended(contextId: string, pc: number, reason: string, state: any): void {
+		if (reason == "Reset") {
+			this.gotoMain(pc);
+		}
+		else {
+			this.sendEvent(new StoppedEvent(reason, this.hashString(contextId), state));
+		}
+	}
+
+	public contextResumed(contextId: string): void {
+		this.sendEvent(new ContinuedEvent(this.hashString(contextId)));
+	}
+
+	public contextException(contextId: string, description: string): void {
+
+	}
+
+	public containerSuspended(contextId: string, pc: number, reason: string, state: any, contextIds: string[]): void {
+
+	}
+
+	public containerResumed(contextIds: string[]): void {
+
+	}
+
+	public contextStateChanged(contextIds: string[]): void {
+
+	}
+
+	private log(message: string): void {
+		this.sendEvent(new OutputEvent(`${message}\n`));
+	}
+
+	private gotoMain(pc: number): void {
+		let expressionsService = <ExpressionsService>this.services["Expressions"];
+		let runControlService = <RunControlService>this.services["RunControlService"];
+		let stackTraceService = <StackTraceService>this.services["StackTrace"];
+		let processesService = <ProcessesService>this.services["Processes"];
+
+		let processContext: IProcessesContext;
+		for (let index in processesService.contexts) {
+			processContext = <IProcessesContext>processesService.contexts[index];
+		}
+
+		let runControlContext: RunControlContext;
+		for (let index in runControlService.contexts) {
+			runControlContext = (<RunControlContext>runControlService.contexts[index]);
+		}
+
+		stackTraceService.getChildren(processContext.ID, (children) => {
+			expressionsService.compute(children.shift(), "C", "main", (expressionContext) => {
+				let address = parseInt(expressionContext.Val.replace("0x", ""), 16);
+				expressionContext.dispose();
+
+				runControlContext.resume(ResumeMode.ReverseUntilActive, address - pc)
+
+			});
+		});
+
+
+
 
 	}
 }
